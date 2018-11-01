e90784e1244c84337fe1365766dd9aaac7ec9c64
gerrkoff <antonkofa@gmail.com>
2018-11-01 22:26:01 +0400

    remove unnecessary

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import App from './App'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <div>Hello World!</div>,
+    document.getElementById('root') as HTMLElement
 )
 registerServiceWorker()
