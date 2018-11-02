6c96eb048fef037c6c39a74acc52885d3a10908e
gerrkoff <antonkofa@gmail.com>
2018-11-02 13:16:56 +0400

    moving

---
+++ client/src/components/overview/NoDateCard.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { List, Segment } from 'semantic-ui-react'
+import { Task } from '../../models'
+import '../../styles/no-date-card.css'
+
+interface IProps {
+    tasks: Task[]
+}
+export class NoDateCard extends React.Component<IProps> {
+    public render() {
+        this.props.tasks.sort((x, y) => x.order > y.order ? 1 : 0)
+        return (
+            <Segment raised inverted id='no-date-card'>
+                <List bulleted className='no-date-card-tasks-view dragula-container' data-id={0}>
+                    {this.props.tasks.map(x =>
+                        <List.Item key={x.id} data-id={x.id}>{x.title}</List.Item>
+                    )}
+                </List>
+            </Segment>
+        )
+    }
+}
--- client/src/components/overview/index.ts
+++ client/src/components/overview/index.ts
@@ -1,3 +1,4 @@
 export * from './Overview'
 export * from './DayCard'
 export * from './DaysBlock'
+export * from './NoDateCard'
