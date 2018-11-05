abeeeccc7f62385209197b6231c66381140ea9bf
gerrkoff <antonkofa@gmail.com>
2018-11-06 02:31:11 +0400

    back

---
+++ DarkDeeds/DarkDeeds.Services/Implementation/TaskService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DarkDeeds.Models;
+
+namespace DarkDeeds.Services.Interface
+{
+    public class TaskService : ITaskService
+    {
+        public async Task<IEnumerable<TaskDto>> SaveTasksAsync(ICollection<TaskDto> tasks)
+        {
+            return await Task.FromResult(tasks);
+        }
+    }
+}
