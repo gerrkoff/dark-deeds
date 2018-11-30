5e9d30c48703f6bb928e9a1076434c7b72ad6331
gerrkoff <antonkofa@gmail.com>
2018-11-30 20:45:06 +0300

    adding dal

---
+++ DarkDeeds/DarkDeeds.Data/Migrations/20181130174405_N001_Init.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DarkDeeds.Data.Migrations
+{
+    public partial class N001_Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
