2b0f4d759d9a93c8577fa1065408f3b802f970cf
gerrkoff <antonkofa@gmail.com>
2018-12-03 23:38:52 +0300

    fix bugs with saving

--- DarkDeeds/DarkDeeds.Data/Repository/Repository.cs
+++ DarkDeeds/DarkDeeds.Data/Repository/Repository.cs
@@ -33,6 +33,11 @@ namespace DarkDeeds.Data.Repository
             {
                 Entities.Add(entity);
             }
+            else
+            {
+                Entities.Attach(entity);
+                _context.Entry(entity).State = EntityState.Modified;
+            }
 
             await _context.SaveChangesAsync();
         }
