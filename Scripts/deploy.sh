f12818416b8e44480ceb5405933e6603b6cf289f
Anton Prokofiev <anton.prokofiev@team.wrike.com>
2019-03-15 19:16:42 +0300

    work on deploy script

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,3 +1,12 @@
+ STATUS=$(git status)
+ if [[ $STATUS != *"Your branch is up to date"* ]] || [[ $STATUS != *"nothing to commit, working tree clean"* ]]
+ then
+    echo Current branch is not clean
+    exit 1
+else
+    echo Current branch is clean, move on
+fi
+
 git checkout staging
 git pull origin staging
 git merge origin/master
--- Scripts/test.sh
+++
@@ -1,8 +0,0 @@
- STATUS=$(git status)
- if [[ $STATUS != *"Your branch is up to date"* ]] || [[ $STATUS != *"nothing to commit, working tree clean"* ]]
- then
-    echo Current branch is not clean
-    exit 1
-else
-    echo Cool
-fi
