a72522e00545816dcabaa34f82ccd449ddca6b4e
gerrkoff <antonkofa@gmail.com>
2019-03-15 23:07:41 +0300

    fix script

--- Scripts/bump-version.sh
+++ Scripts/bump-version.sh
@@ -2,4 +2,4 @@ echo "UPDATING VERSION"
 echo "Enter version (PATCH): " 
 read version
 
-dotnet dotnet-bumpversion/BumpVersion.dll /Users/anton.prokofiev/dark-deeds/DarkDeeds/DarkDeeds.Api/DarkDeeds.Api.csproj $version
+dotnet dotnet-bumpversion/BumpVersion.dll ../DarkDeeds/DarkDeeds.Api/DarkDeeds.Api.csproj $version
