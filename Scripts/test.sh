16b0f12fd9524e500ddf2d3b4ed7811bb3080915
Anton Prokofiev <anton.prokofiev@team.wrike.com>
2019-03-15 19:09:48 +0300

    test

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,2 +1,8 @@
- BRANCH=$(git status)
- echo $BRANCH
+ STATUS=$(git status)
+ if [ "$STATUS" != "On branch master Your branch is up to date with 'origin/master'. nothing to commit, working tree clean" ]
+ then
+    echo Current branch is not clean
+    exit 1
+else
+    echo Cool
+fi
