bc8cb49a7188c06a0496d3e21c418a2def9689d0
Anton Prokofiev <anton.prokofiev@team.wrike.com>
2019-03-28 20:52:41 +0300

    adjust script

--- Scripts/db-script.sh
+++ Scripts/db-script.sh
@@ -1,3 +1,4 @@
 echo "Enter start migration name: " 
 read name
+echo
 dotnet ef migrations script $name -p ../DarkDeeds/DarkDeeds.Data/DarkDeeds.Data.csproj -s ../DarkDeeds/DarkDeeds.Api/DarkDeeds.Api.csproj
