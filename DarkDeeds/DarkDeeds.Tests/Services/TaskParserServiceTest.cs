7e48123d214b24d00460b98a0763fbc736c4512a
gerrkoff <antonkofa@gmail.com>
2019-03-29 09:52:27 +0300

    copy task parser tests from FE

--- DarkDeeds/DarkDeeds.Tests/Services/TaskParserServiceTest.cs
+++ DarkDeeds/DarkDeeds.Tests/Services/TaskParserServiceTest.cs
@@ -1,3 +1,5 @@
+using System;
+using DarkDeeds.Enums;
 using DarkDeeds.Services.Implementation;
 using Xunit;
 
@@ -12,7 +14,94 @@ namespace DarkDeeds.Tests.Services
 
             var result = service.ParseTask("");
             
-            Assert.True(result != null);
+            Assert.NotNull(result);
         }
+        
+        [Fact]
+        public void ParseTask_ReturnTaskWithNoDateAndNoTime()
+        {
+            var service = new TaskParserService();
+
+            var result = service.ParseTask("Test!");
+            
+            Assert.Equal("Test!", result.Title);
+            Assert.Equal(TaskTimeTypeEnum.NoTime, result.TimeType);
+            Assert.Null(result.DateTime);
+        }
+        
+//        [Fact]
+//        public void ParseTask_ReturnTaskWithDateAndNoTime()
+//        {
+//            var service = new TaskParserService();
+//
+//            var result = service.ParseTask("1231 Test!");
+//
+//            var currentYear = DateTime.UtcNow.Year;
+//            Assert.Equal("Test!", result.Title);
+//            Assert.Equal(TaskTimeTypeEnum.NoTime, result.TimeType);
+//            Assert.Equal(new DateTime(currentYear,12, 31, 0, 0, 0),  result.DateTime);
+//        }
+        
+        [Fact]
+        public void ParseTask_ReturnTaskWithDateAndNoTime_NotWorkingWithoutSpace()
+        {
+            var service = new TaskParserService();
+
+            var result = service.ParseTask("0101Test!!!");
+            
+            Assert.Equal("0101Test!!!", result.Title);
+            Assert.Equal(TaskTimeTypeEnum.NoTime, result.TimeType);
+            Assert.Null(result.DateTime);
+        }
+        
+//        [Fact]
+//        public void ParseTask_ReturnTaskWithDateAndTime()
+//        {
+//            var service = new TaskParserService();
+//
+//            var result = service.ParseTask("1231 2359 Test!");
+//            
+//            var currentYear = DateTime.UtcNow.Year;
+//            Assert.Equal("Test!", result.Title);
+//            Assert.Equal(TaskTimeTypeEnum.ConcreteTime, result.TimeType);
+//            Assert.Equal(new DateTime(currentYear,12, 31, 23, 59, 0),  result.DateTime);
+//        }
+        
+//        [Fact]
+//        public void ParseTask_ReturnTaskWithDateAndTime_NotWorkingWithoutSpace()
+//        {
+//            var service = new TaskParserService();
+//
+//            var result = service.ParseTask("0101 0101Test!!!");
+//            
+//            var currentYear = DateTime.UtcNow.Year;
+//            Assert.Equal("0101Test!!!", result.Title);
+//            Assert.Equal(TaskTimeTypeEnum.NoTime, result.TimeType);
+//            Assert.Equal(new DateTime(currentYear,1, 1, 0, 0, 0),  result.DateTime);
+//        }
+        
+//        [Fact]
+//        public void ParseTask_ReturnTaskWithDateAndNoTimeWithYear()
+//        {
+//            var service = new TaskParserService();
+//
+//            var result = service.ParseTask("20170101 Test");
+//            
+//            Assert.Equal("Test", result.Title);
+//            Assert.Equal(TaskTimeTypeEnum.NoTime, result.TimeType);
+//            Assert.Equal(new DateTime(2017,1, 1, 0, 0, 0),  result.DateTime);
+//        }
+        
+//        [Fact]
+//        public void ParseTask_ReturnTaskWithDateAndAfterTimeWithYear()
+//        {
+//            var service = new TaskParserService();
+//
+//            var result = service.ParseTask("20171010 >0211 Test");
+//            
+//            Assert.Equal("Test", result.Title);
+//            Assert.Equal(TaskTimeTypeEnum.AfterTime, result.TimeType);
+//            Assert.Equal(new DateTime(2017,9, 10, 2, 11, 0),  result.DateTime);
+//        }
     }
 }
