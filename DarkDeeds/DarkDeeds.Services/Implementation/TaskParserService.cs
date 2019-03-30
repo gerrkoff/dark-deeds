5a2165c2d8de3f4533d544ed5eb6d5e0e9681d34
gerrkoff <antonkofa@gmail.com>
2019-03-30 15:23:14 +0300

    implement task parser

--- DarkDeeds/DarkDeeds.Services/Implementation/TaskParserService.cs
+++ DarkDeeds/DarkDeeds.Services/Implementation/TaskParserService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Text.RegularExpressions;
+using DarkDeeds.Enums;
 using DarkDeeds.Models;
 using DarkDeeds.Services.Interface;
 
@@ -10,25 +11,82 @@ namespace DarkDeeds.Services.Implementation
 {
     public class TaskParserService : ITaskParserService
     {
-        // TODO: implement this
-        // TODO: unit-tests
         public TaskDto ParseTask(string task, int timeAdjustment = 0)
         {
             var taskDto = new TaskDto();
+            task = ParseDate(task, out int year, out int month, out int day, out bool withDate);
+            task = ParseTime(task, out int hour, out int minutes, out TaskTimeTypeEnum timeType);
             
-            var dateRx = new Regex(@"^\d{4}\s");
-            if (dateRx.IsMatch(task))
+            taskDto.Title = task;
+            taskDto.TimeType = timeType;
+            if (withDate)
+                taskDto.DateTime = CreateDateTime(year, month, day, hour, minutes, timeAdjustment);
+            return taskDto;
+        }
+
+        private string ParseTime(string task, out int hour, out int minutes, out TaskTimeTypeEnum timeType)
+        {
+            var timeAfterRx = new Regex(@"^>\d{4}\s");
+            var timeRx = new Regex(@"^\d{4}\s");
+            string time = string.Empty;
+            hour = 0;
+            minutes = 0;
+            timeType = TaskTimeTypeEnum.NoTime;
+            
+            if (timeAfterRx.IsMatch(task))
             {
-                string date = task.Substring(0, 4);
+                time = task.Substring(1, 4);
+                task = task.Substring(6);
+                timeType = TaskTimeTypeEnum.AfterTime;
+            }
+            else if (timeRx.IsMatch(task))
+            {
+                time = task.Substring(0, 4);
                 task = task.Substring(5);
-                
-                int month = int.Parse(date.Substring(0, 2));
-                int day = int.Parse(date.Substring(2, 2));
-                taskDto.DateTime = CreateDateTime(DateTime.UtcNow.Year, month, day, 0, 0, timeAdjustment);
+                timeType = TaskTimeTypeEnum.ConcreteTime;
             }
 
-            taskDto.Title = task;
-            return taskDto;
+            if (!string.IsNullOrEmpty(time))
+            {
+                hour = int.Parse(time.Substring(0, 2));
+                minutes = int.Parse(time.Substring(2, 2));
+            }
+
+            return task;
+        }
+
+        private string ParseDate(string task, out int year, out int month, out int day, out bool withDate)
+        {
+            var dateWithYearRx = new Regex(@"^\d{8}\s");
+            var dateRx = new Regex(@"^\d{4}\s");
+            string date = string.Empty;
+            year = 0;
+            month = 0;
+            day = 0;
+            withDate = false;
+                        
+            if (dateWithYearRx.IsMatch(task))
+            {
+                date = task.Substring(4, 4);
+                year = int.Parse(task.Substring(0, 4));
+                task = task.Substring(9);
+
+            }
+            else if (dateRx.IsMatch(task))
+            {
+                date = task.Substring(0, 4);
+                task = task.Substring(5);
+                year = DateTime.UtcNow.Year;
+            }
+            
+            if (!string.IsNullOrEmpty(date))
+            {
+                month = int.Parse(date.Substring(0, 2));
+                day = int.Parse(date.Substring(2, 2));
+                withDate = true;
+            }
+            
+            return task;
         }
 
         private DateTime CreateDateTime(int year, int month, int day, int hour, int minutes, int timeAdjustment)
