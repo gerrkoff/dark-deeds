df661f36d344620df0e4451c84005f6946bfe035
gerrkoff <antonkofa@gmail.com>
2019-05-06 22:41:33 +0300

    add logging to signalr

--- DarkDeeds.WebClient/src/api/task-hub.ts
+++ DarkDeeds.WebClient/src/api/task-hub.ts
@@ -7,11 +7,13 @@ const connection = new signalR.HubConnectionBuilder()
     .withUrl(baseUrl + 'ws/task', {
         accessTokenFactory: () => StorageService.loadAccessToken() as string
     })
+    .configureLogging(signalR.LogLevel.Information)
     .build()
 
 const service = {
     hubStart(): Promise<void> {
         return connection.start()
+            .catch(error => console.error('!!!', error.toString()))
     },
 
     hubStop(): Promise<void> {
