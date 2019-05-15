a2255e09bba95944746a187d2aef6396d4a13bfe
gerrkoff <antonkofa@gmail.com>
2019-05-15 20:02:32 +0300

    add migration with removing After Time

---
+++ DarkDeeds/DarkDeeds.Data/Migrations/20190515164201_N005_Migrate_TimeType_to_no_AfterTime.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DarkDeeds.Data.Migrations
+{
+    public partial class N005_Migrate_TimeType_to_no_AfterTime : Migration
+    {
+        private const string TableTasks = "Tasks";
+        private const string ColumnType = "TimeType";
+        
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(TableTasks, ColumnType, 2, ColumnType, 1);
+            migrationBuilder.UpdateData(TableTasks, ColumnType, 3, ColumnType, 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
