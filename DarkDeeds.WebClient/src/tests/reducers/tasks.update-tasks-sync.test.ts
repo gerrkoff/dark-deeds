1cd8b6bb09a35f78935424c05e275ffbc96ac9a6
Anton Prokofiev <anton.prokofiev@team.wrike.com>
2019-05-17 11:55:00 +0300

    add tests

--- DarkDeeds.WebClient/src/tests/reducers/tasks.update-tasks-sync.test.ts
+++ DarkDeeds.WebClient/src/tests/reducers/tasks.update-tasks-sync.test.ts
@@ -1,149 +1,91 @@
-// import { tasks as taskReducer } from '../../redux/reducers/tasks'
-// import { ITasksState } from '../../redux/types'
-// import { Task, TaskLoadingStateEnum } from '../../models'
-// import * as actions from '../../redux/constants/tasks'
+import { tasks as taskReducer } from '../../redux/reducers/tasks'
+import { ITasksState } from '../../redux/types'
+import { Task, TaskLoadingStateEnum } from '../../models'
+import * as actions from '../../redux/constants/tasks'
 
-// function createState(
-//     tasks: Task[] = [],
-//     loadingState: TaskLoadingStateEnum = TaskLoadingStateEnum.Loading,
-//     saving: boolean = false,
-//     changed: boolean = false,
-//     hubReconnecting: boolean = false
-//     ): ITasksState {
-//     return { loadingState, saving, tasks, changed, hubReconnecting, hubHeartbeatLastTime: new Date() }
-// }
+function createState(
+    tasks: Task[] = [],
+    loadingState: TaskLoadingStateEnum = TaskLoadingStateEnum.Loading,
+    saving: boolean = false,
+    changed: boolean = false,
+    hubReconnecting: boolean = false
+    ): ITasksState {
+    return { loadingState, saving, tasks, changed, hubReconnecting, hubHeartbeatLastTime: new Date() }
+}
 
-// function createTask(clientId: number = 1, id: number = 1, title: string = '', deleted: boolean = false, version: number = 0): Task {
-//     const task = new Task(clientId, title)
-//     task.id = id
-//     task.deleted = deleted
-//     task.version = version
-//     return task
-// }
+function createTask(clientId: number = 1, id: number = 1, title: string = '', version: number = 0, changed: boolean = false): Task {
+    const task = new Task(clientId, title)
+    task.id = id
+    task.version = version
+    task.changed = changed
+    return task
+}
 
-// test('[TASKS_UPDATE_TASKS] (non local update) add new task - should be added to the list', () => {
-//     const state = createState()
-//     const action: actions.ITasksUpdateTasks = {
-//         type: actions.TASKS_UPDATE_TASKS,
-//         tasks: [createTask(-10, 10, 'qwerty')],
-//         localUpdate: false
-//     }
-//     const result = taskReducer(state, action)
+test('[TASKS_UPDATE_TASKS_SYNC] just return new tasks if no local', () => {
+    const state = createState()
+    const action: actions.ITasksUpdateTasksSync = {
+        type: actions.TASKS_UPDATE_TASKS_SYNC,
+        tasks: [createTask(1, 1, '1'), createTask(2, 2, '2')]
+    }
+    const result = taskReducer(state, action)
 
-//     expect(result.tasks.length).toBe(1)
-//     expect(result.tasks[0].clientId).toBe(10)
-//     expect(result.tasks[0].id).toBe(10)
-//     expect(result.tasks[0].title).toBe('qwerty')
-// })
+    expect(result.tasks.length).toBe(2)
+    expect(result.tasks[0].clientId).toBe(1)
+    expect(result.tasks[0].id).toBe(1)
+    expect(result.tasks[0].title).toBe('1')
+    expect(result.tasks[1].clientId).toBe(2)
+    expect(result.tasks[1].id).toBe(2)
+    expect(result.tasks[1].title).toBe('2')
+})
 
-// test('[TASKS_UPDATE_TASKS] (non local update) add new task with the same clientId - should be both in the list', () => {
-//     const state = createState([createTask(-10, 0, 'local')])
-//     const action: actions.ITasksUpdateTasks = {
-//         type: actions.TASKS_UPDATE_TASKS,
-//         tasks: [createTask(-10, 10, 'pushed')],
-//         localUpdate: false
-//     }
-//     const result = taskReducer(state, action)
+test('[TASKS_UPDATE_TASKS_SYNC] keep locally created tasks', () => {
+    const localTask = createTask(-1, 0, 'qqq')
+    const state = createState([localTask])
+    const action: actions.ITasksUpdateTasksSync = {
+        type: actions.TASKS_UPDATE_TASKS_SYNC,
+        tasks: []
+    }
+    const result = taskReducer(state, action)
 
-//     expect(result.tasks.length).toBe(2)
-//     const task1 = result.tasks.find(x => x.clientId === -10)
-//     expect(task1).not.toBeNull()
-//     expect(task1!.id).toBe(0)
-//     expect(task1!.title).toBe('local')
-//     const task2 = result.tasks.find(x => x.clientId === 10)
-//     expect(task2).not.toBeNull()
-//     expect(task2!.id).toBe(10)
-//     expect(task2!.title).toBe('pushed')
-// })
+    expect(result.tasks.length).toBe(1)
+    expect(result.tasks[0]).toBe(localTask)
+})
 
-// test('[TASKS_UPDATE_TASKS] (non local update) delete task with existing clientId - should be removed from the list', () => {
-//     const state = createState([createTask(1)])
-//     const action: actions.ITasksUpdateTasks = {
-//         type: actions.TASKS_UPDATE_TASKS,
-//         tasks: [createTask(1, 1, '', true)],
-//         localUpdate: false
-//     }
-//     const result = taskReducer(state, action)
+test('[TASKS_UPDATE_TASKS_SYNC] remove task if it is not created locally and not received', () => {
+    const state = createState([createTask()])
+    const action: actions.ITasksUpdateTasksSync = {
+        type: actions.TASKS_UPDATE_TASKS_SYNC,
+        tasks: []
+    }
+    const result = taskReducer(state, action)
 
-//     expect(result.tasks.length).toBe(0)
-// })
+    expect(result.tasks.length).toBe(0)
+})
 
-// test('[TASKS_UPDATE_TASKS] (non local update) delete task with non-existing clientId - should be ignored', () => {
-//     const state = createState([createTask(2)])
-//     const action: actions.ITasksUpdateTasks = {
-//         type: actions.TASKS_UPDATE_TASKS,
-//         tasks: [createTask(1, 1, '', true)],
-//         localUpdate: false
-//     }
-//     const result = taskReducer(state, action)
+test('[TASKS_UPDATE_TASKS_SYNC] keep local task if version matches, changed should be kept', () => {
+    const localTask = createTask(1, 1, 'qqq', 10, true)
+    const state = createState([localTask])
+    const action: actions.ITasksUpdateTasksSync = {
+        type: actions.TASKS_UPDATE_TASKS_SYNC,
+        tasks: [createTask(1, 1, 'www', 10)]
+    }
+    const result = taskReducer(state, action)
 
-//     expect(result.tasks.length).toBe(1)
-// })
+    expect(result.tasks.length).toBe(1)
+    expect(result.tasks[0]).toBe(localTask)
+    expect(result.changed).toBe(true)
+})
 
-// test('[TASKS_UPDATE_TASKS] (non local update) update task - should be updated & reset updated flag', () => {
-//     const state = createState([createTask(1, 1, 'qqq')])
-//     const action: actions.ITasksUpdateTasks = {
-//         type: actions.TASKS_UPDATE_TASKS,
-//         tasks: [createTask(1, 1, 'www')],
-//         localUpdate: false
-//     }
-//     const result = taskReducer(state, action)
+test('[TASKS_UPDATE_TASKS_SYNC] update local task if version does not matches, change should be reset', () => {
+    const state = createState([createTask(1, 1, 'qqq', 10, true)])
+    const action: actions.ITasksUpdateTasksSync = {
+        type: actions.TASKS_UPDATE_TASKS_SYNC,
+        tasks: [createTask(1, 1, 'www', 5)]
+    }
+    const result = taskReducer(state, action)
 
-//     expect(result.tasks.length).toBe(1)
-//     expect(result.tasks[0].title).toBe('www')
-//     expect(result.tasks[0].changed).toBe(false)
-// })
-
-// test('[TASKS_UPDATE_TASKS] (local update) update task - should not be updated', () => {
-//     const state = createState([createTask(1, 1, 'qqq')])
-//     const action: actions.ITasksUpdateTasks = {
-//         type: actions.TASKS_UPDATE_TASKS,
-//         tasks: [createTask(1, 1, 'www')],
-//         localUpdate: true
-//     }
-//     const result = taskReducer(state, action)
-
-//     expect(result.tasks.length).toBe(1)
-//     expect(result.tasks[0].title).toBe('qqq')
-// })
-
-// test('[TASKS_UPDATE_TASKS] (local update) update task - should update clientId with id and version with new version', () => {
-//     const state = createState([createTask(-100, 0, '', false, 11)])
-//     const action: actions.ITasksUpdateTasks = {
-//         type: actions.TASKS_UPDATE_TASKS,
-//         tasks: [createTask(-100, 20, '', false, 33)],
-//         localUpdate: true
-//     }
-//     const result = taskReducer(state, action)
-
-//     expect(result.tasks.length).toBe(1)
-//     expect(result.tasks[0].id).toBe(20)
-//     expect(result.tasks[0].clientId).toBe(20)
-//     expect(result.tasks[0].version).toBe(33)
-// })
-
-// test('[TASKS_UPDATE_TASKS] (local update) update task - should set updated flag to true if not equal to saved state', () => {
-//     const state = createState([createTask(1, 1, 'qqq')])
-//     const action: actions.ITasksUpdateTasks = {
-//         type: actions.TASKS_UPDATE_TASKS,
-//         tasks: [createTask(1, 1, 'www')],
-//         localUpdate: true
-//     }
-//     const result = taskReducer(state, action)
-
-//     expect(result.tasks.length).toBe(1)
-//     expect(result.tasks[0].changed).toBeTruthy()
-// })
-
-// test('[TASKS_UPDATE_TASKS] (local update) update task - should set updated flag to false if equal to saved state', () => {
-//     const state = createState([createTask(1, 1, 'qqq')])
-//     const action: actions.ITasksUpdateTasks = {
-//         type: actions.TASKS_UPDATE_TASKS,
-//         tasks: [createTask(1, 1, 'qqq')],
-//         localUpdate: true
-//     }
-//     const result = taskReducer(state, action)
-
-//     expect(result.tasks.length).toBe(1)
-//     expect(result.tasks[0].changed).toBeFalsy()
-// })
+    expect(result.tasks.length).toBe(1)
+    expect(result.tasks[0].title).toBe('www')
+    expect(result.tasks[0].version).toBe(5)
+    expect(result.changed).toBe(false)
+})
