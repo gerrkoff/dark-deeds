91d5da8fb121d3d6a9887680b9085f89e91692d6
gerrkoff <antonkofa@gmail.com>
2019-06-16 15:08:48 +0300

    add publish script

---
+++ Deploy/build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+print () {
+   printf "\n\e[46m\e[1m $1 \e[0m\e[0m\n\n"
+}
+
+# cleanup
+print 'CLEAN'
+rm -rf src
+rm -rf test-results
+echo cleaned
+
+# test & build FE
+print 'FE: GET DEPS'
+cd ../DarkDeeds.WebClient/
+npm install || exit $?
+
+print 'FE: TEST'
+npm run test-ci || exit $?
+
+print 'FE: BUILD'
+npm run build || exit $?
+
+# test & build BE
+print 'BE: TEST'
+cd ../DarkDeeds/DarkDeeds.Tests/
+dotnet test "--logger:trx;LogFileName=results.trx" --results-directory ../../Deploy/test-results || exit $?
+
+print 'BE: SET BUILD VERSION'
+cd ../DarkDeeds.Api/
+dotnet ../../Deploy/dotnet-setversion/dotnet-setversion.dll vs $1
+
+print 'BE: BUILD'
+dotnet publish -c Release -o ../../Deploy/src || exit $?
