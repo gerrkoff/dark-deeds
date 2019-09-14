8921cdf6964cbad9d6f7264e783aaf5c4e6ef523
gerrkoff <antonkofa@gmail.com>
2019-09-14 21:32:57 +0300

    add db migration

--- DarkDeeds/DarkDeeds.Data/Migrations/20190914163948_N001_Add_Time_to_TaskEntity.Designer.cs
+++ DarkDeeds/DarkDeeds.Data/Migrations/20190914163948_N001_Add_Time_to_TaskEntity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using DarkDeeds.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace DarkDeeds.Data.Migrations
 {
     [DbContext(typeof(DarkDeedsContext))]
-    partial class DarkDeedsContextModelSnapshot : ModelSnapshot
+    [Migration("20190914163948_N001_Add_Time_to_TaskEntity")]
+    partial class N001_Add_Time_to_TaskEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DarkDeeds/DarkDeeds.Data/Migrations/20190914163948_N001_Add_Time_to_TaskEntity.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DarkDeeds.Data.Migrations
+{
+    public partial class N001_Add_Time_to_TaskEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Tasks",
+                newName: "Date");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Time",
+                table: "Tasks",
+                nullable: true);
+
+            migrationBuilder.Sql(@"
+UPDATE ""Tasks""
+SET ""Time"" = CASE WHEN
+                    date_part('hour', ""Date"") <> 0 OR
+                    date_part('minute', ""Date"") <> 0
+                THEN date_part('hour', ""Date"") * 60 + date_part('minute', ""Date"")
+                ELSE NULL END,
+""Date"" = date_trunc('day', ""Date"");
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Tasks");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Tasks",
+                newName: "DateTime");
+        }
+    }
+}
