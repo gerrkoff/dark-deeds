9cb5286162c142856f9a4b3736ff62e30a43acb6
gerrkoff <antonkofa@gmail.com>
2019-09-26 00:13:26 +0300

    wip

--- Scripts/ci-build.sh
+++ Scripts/ci-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd ..
-rm -rf Deploy/artifacts
-docker build -t dark-deeds-builder -f Deploy/dockerfile-build .
+rm -rf CI/artifacts
+docker build -t dark-deeds-builder -f CI/dockerfile-build .
 docker rm -f dark-deeds-builder
-docker run -v "$(pwd)"/Deploy/artifacts:/app/Deploy/artifacts --name dark-deeds-builder dark-deeds-builder
+docker run -v "$(pwd)"/CI/artifacts:/app/CI/artifacts --name dark-deeds-builder dark-deeds-builder
 docker rm -f dark-deeds-builder 
--- Scripts/ci-test-e2e.sh
+++ Scripts/ci-test-e2e.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd ..
-rm -rf Deploy/artifacts
-docker build -t dark-deeds-test-e2e -f Deploy/dockerfile-test-e2e .
+rm -rf CI/artifacts
+docker build -t dark-deeds-test-e2e -f CI/dockerfile-test-e2e .
 docker rm -f dark-deeds-test-e2e
-docker run -v "$(pwd)"/Deploy/artifacts:/app/artifacts --name dark-deeds-test-e2e dark-deeds-test-e2e
+docker run -v "$(pwd)"/CI/artifacts:/app/artifacts --name dark-deeds-test-e2e dark-deeds-test-e2e
 docker rm -f dark-deeds-test-e2e
