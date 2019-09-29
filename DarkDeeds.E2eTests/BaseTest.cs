04f1bd485dbc869c0ae8bc51e539ecec47f77751
gerrkoff <antonkofa@gmail.com>
2019-09-29 23:42:57 +0300

    add version test/output

--- DarkDeeds.E2eTests/BaseTest.cs
+++ DarkDeeds.E2eTests/BaseTest.cs
@@ -11,13 +11,13 @@ namespace DarkDeeds.E2eTests
 {
     public class BaseTest
     {
-        private static readonly string Url = Environment.GetEnvironmentVariable("URL") ?? "http://localhost:3000";
         private static readonly bool RunContainer = bool.Parse(Environment.GetEnvironmentVariable("RUN_CONTAINER") ?? "false");
         private static readonly string ArtifactsPath = Environment.GetEnvironmentVariable("ARTIFACTS_PATH") ?? string.Empty;
+        protected static readonly string Url = Environment.GetEnvironmentVariable("URL") ?? "http://localhost:3000";
         protected static readonly string Username = Environment.GetEnvironmentVariable("USERNAME") ?? "qqq";
         protected static readonly string Password = Environment.GetEnvironmentVariable("PASSWORD") ?? "qqq";
         
-        private static Random _random = new Random();
+        private static readonly Random Random = new Random();
 
         private RemoteWebDriver CreateDriver()
         {
@@ -52,7 +52,7 @@ namespace DarkDeeds.E2eTests
 
         protected string RandomizeText(string text)
         {
-            return $"{text} {_random.Next()}";
+            return $"{text} {Random.Next()}";
         }
     }
 }
--- DarkDeeds.E2eTests/SmokeTests.cs
+++ DarkDeeds.E2eTests/SmokeTests.cs
@@ -1,10 +1,30 @@
-﻿using DarkDeeds.E2eTests.Extensions;
+﻿using System.Net.Http;
+using DarkDeeds.E2eTests.Extensions;
+using Newtonsoft.Json.Linq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace DarkDeeds.E2eTests
 {
     public class SmokeTests : BaseTest
     {
+        private readonly ITestOutputHelper _output;
+
+        public SmokeTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+        
+        [Fact]
+        public async void GetBuildVersionTest()
+        {
+            var httpClient = new HttpClient();
+            var url = $"{Url}/api/build-info";
+            var result = await httpClient.GetStringAsync(url);
+            var version = (string) JObject.Parse(result)["version"];
+            _output.WriteLine(version);
+        }
+
         [Fact]
         public void SignInTest()
         {
