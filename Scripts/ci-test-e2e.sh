03025577121c37e9ae59d928e7902878add49d02
gerrkoff <antonkofa@gmail.com>
2019-10-03 10:00:43 +0300

    adjust e2e test

--- Scripts/ci-test-e2e.sh
+++ Scripts/ci-test-e2e.sh
@@ -7,10 +7,9 @@ docker run \
     -e TZ=America/New_York \
     -e RUN_CONTAINER='true' \
     -e ARTIFACTS_PATH='/app/artifacts' \
-    -e API_URL='http://localhost:5000' \
-    -e URL='http://localhost:3000' \
-    -e USERNAME='qqq' \
-    -e PASSWORD='qqq' \
+    -e URL='https://dark-deeds.grkf.ru' \
+    -e USERNAME='' \
+    -e PASSWORD='' \
     -v "$(pwd)"/CI/artifacts:/app/artifacts \
     --name dark-deeds-test-e2e \
     dark-deeds-test-e2e
