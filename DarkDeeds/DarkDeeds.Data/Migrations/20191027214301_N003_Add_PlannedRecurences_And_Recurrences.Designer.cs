33f460261797d5b2c5db8aea3eae4ae451dfbc92
gerrkoff <antonkofa@gmail.com>
2019-10-28 00:45:05 +0300

    add migration

--- DarkDeeds/DarkDeeds.Data/Migrations/20191027214301_N003_Add_PlannedRecurences_And_Recurrences.Designer.cs
+++ DarkDeeds/DarkDeeds.Data/Migrations/20191027214301_N003_Add_PlannedRecurences_And_Recurrences.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using DarkDeeds.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace DarkDeeds.Data.Migrations
 {
     [DbContext(typeof(DarkDeedsContext))]
-    partial class DarkDeedsContextModelSnapshot : ModelSnapshot
+    [Migration("20191027214301_N003_Add_PlannedRecurences_And_Recurrences")]
+    partial class N003_Add_PlannedRecurences_And_Recurrences
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
