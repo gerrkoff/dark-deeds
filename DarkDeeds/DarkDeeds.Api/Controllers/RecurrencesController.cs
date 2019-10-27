557883ff0d7f53901abb0911d73104a7f0b8d321
gerrkoff <antonkofa@gmail.com>
2019-10-28 00:55:23 +0300

    add api

---
+++ DarkDeeds/DarkDeeds.Api/Controllers/RecurrencesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using DarkDeeds.Api.Controllers.Base;
+using DarkDeeds.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DarkDeeds.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecurrencesController : BaseUserController
+    {
+        private readonly IRecurrenceCreatorService _recurrenceCreatorService;
+
+        public RecurrencesController(IRecurrenceCreatorService recurrenceCreatorService)
+        {
+            _recurrenceCreatorService = recurrenceCreatorService;
+        }
+
+        [Route("create")]
+        [HttpPost]
+        public Task Create()
+        {
+            return _recurrenceCreatorService.CreateAsync();
+        }
+    }
+}
