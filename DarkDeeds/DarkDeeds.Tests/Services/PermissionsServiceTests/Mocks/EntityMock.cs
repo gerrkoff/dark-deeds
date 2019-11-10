30ee16067f62891c814efb9783cdca7cb3053eb5
gerrkoff <antonkofa@gmail.com>
2019-11-10 14:18:49 +0300

    wip

---
+++ DarkDeeds/DarkDeeds.Tests/Services/PermissionsServiceTests/Mocks/EntityMock.cs
@@ -0,0 +1,9 @@
+﻿using DarkDeeds.Data.Entity.Base;
+
+namespace DarkDeeds.Tests.Services.PermissionsServiceTests.Mocks
+{
+    public class EntityMock : BaseEntity, IUserOwnedEntity
+    {
+        public string UserId { get; set; }
+    }
+}
---
+++ DarkDeeds/DarkDeeds.Tests/Services/PermissionsServiceTests/PermissionsServiceTest.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using DarkDeeds.Common.Exceptions;
+using DarkDeeds.Services.Implementation;
+using DarkDeeds.Tests.Services.PermissionsServiceTests.Mocks;
+using Xunit;
+
+namespace DarkDeeds.Tests.Services.PermissionsServiceTests
+{
+    public class PermissionsServiceTest : BaseTest
+    {        
+        [Fact]
+        public async Task CheckIfUserCanEditEntitiesAsync_ThrowsNoExceptionIfDtosAreValid()
+        {
+            var dtos = new[] {new DtoMock {Id = 1}, new DtoMock {Id = 2}};
+            var repo = Helper.CreateRepoNonDeletableMock(
+                new EntityMock {Id = 1, UserId = "userid1"},
+                new EntityMock {Id = 2, UserId = "userid1"});
+            
+            var service = new PermissionsService();
+
+            await service.CheckIfUserCanEditEntitiesAsync(dtos, repo.Object, "userid1", "");
+        }
+        
+        [Fact]
+        public async Task CheckIfUserCanEditEntitiesAsync_ThrowsExceptionIfDtosAreNotValid()
+        {
+            var dtos = new[] {new DtoMock {Id = 1}, new DtoMock {Id = 2}};
+            var repo = Helper.CreateRepoNonDeletableMock(
+                new EntityMock {Id = 1, UserId = "userid1"},
+                new EntityMock {Id = 2, UserId = "userid2"});
+            
+            var service = new PermissionsService();
+
+            await Assert.ThrowsAsync<ServiceException>(() =>
+                service.CheckIfUserCanEditEntitiesAsync(dtos, repo.Object, "userid1", ""));
+        }
+    }
+}
