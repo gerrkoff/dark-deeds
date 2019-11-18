fc841516fe6f4a44bbf763b66fe9e17d06230cc7
gerrkoff <antonkofa@gmail.com>
2019-11-18 03:07:08 +0300

    wip

--- Scripts/run-be.sh
+++ Scripts/run-be.sh
@@ -1,8 +1,16 @@
+if docker network ls | grep -q dark-deeds-nw
+then 
+   echo "Network has been created already"
+else
+   docker network create -d bridge dark-deeds-nw
+fi
+
 cd ..
 docker build -t dark-deeds-be -f Scripts/run/be.dockerfile . || exit $?
 docker rm -f dark-deeds-be
 docker run -t \
     -p 5000:80 \
     -e ASPNETCORE_ENVIRONMENT=Development \
+    --network=dark-deeds-nw \
     --name dark-deeds-be \
     dark-deeds-be
--- Scripts/run-db.sh
+++ Scripts/run-db.sh
@@ -1,7 +1,15 @@
+if docker network ls | grep -q dark-deeds-nw
+then 
+   echo "Network has been created already"
+else
+   docker network create -d bridge dark-deeds-nw
+fi
+
 docker rm -f dark-deeds-db
 docker run -d \
     -p 5432:5432 \
     -v dark-deeds-psql-volume:/var/lib/postgresql/data \
     -e POSTGRES_PASSWORD=password \
+    --network=dark-deeds-nw \
     --name dark-deeds-db \
     postgres:12.0
