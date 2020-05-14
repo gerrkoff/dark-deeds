8aa9201b54e3dc5a58349a67f1f8ab80a9c77276
gerrkoff <antonkofa@gmail.com>
2020-05-14 23:39:27 +0300

    add dark theme

--- DarkDeeds.WebClient/src/di/services/appearance-service.ts
+++ DarkDeeds.WebClient/src/di/services/appearance-service.ts
@@ -20,7 +20,19 @@ export class AppearanceService {
             return
         }
 
-        if (theme === AppearanceThemeEnum.Light) {
+        if (theme === AppearanceThemeEnum.Dark) {
+            html.style.setProperty('--clr-base', '#555')
+            html.style.setProperty('--clr-main', '#131313')
+            html.style.setProperty('--clr-help', '#323232')
+            html.style.setProperty('--clr-help-2', '#444446')
+            html.style.setProperty('--clr-text-primary', '#fdfdfd')
+            html.style.setProperty('--clr-text-secondary', '#646464')
+            html.style.setProperty('--clr-shadow', '#353A45')
+            html.style.setProperty('--clr-success', '#058909')
+            html.style.setProperty('--clr-error', '#B43B2F')
+            html.style.setProperty('--clr-warn', '#BE9B0C')
+            html.style.setProperty('--clr-info', '#2875A8')
+        } else {
             html.style.setProperty('--clr-base', '#f2f3f4')
             html.style.setProperty('--clr-main', 'white')
             html.style.setProperty('--clr-help', 'gainsboro')
