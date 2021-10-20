102bc91e4cfe5846b349434f8f8d615c8f07179f
gerrkoff <antonkofa@gmail.com>
2021-10-21 00:09:06 +0200

    wip

--- ci/build.sh
+++ ci/build.sh
@@ -14,22 +14,22 @@ fi
 export BUILD_VERSION=$BUILD_VERSION
 echo BUILD_VERSION=$BUILD_VERSION
 
-docker-compose \
-    -f "${DOCKER_COMPOSE_FILE}" \
-    build || exit $?
+# docker-compose \
+#     -f "${DOCKER_COMPOSE_FILE}" \
+#     build || exit $?
 
-IMAGES=$(cat ${DOCKER_COMPOSE_FILE} | grep 'image: ' | cut -d':' -f 2 | tr -d '"')
-for IMAGE in $IMAGES
-do
-    if [ "$1" = "push" ]; then
-        docker push "${IMAGE}":"${BUILD_VERSION}"
+# IMAGES=$(cat ${DOCKER_COMPOSE_FILE} | grep 'image: ' | cut -d':' -f 2 | tr -d '"')
+# for IMAGE in $IMAGES
+# do
+#     if [ "$1" = "push" ]; then
+#         docker push "${IMAGE}":"${BUILD_VERSION}"
 
-        if [ "$BRANCH" = "$DEPLOY_BRANCH" ]; then
-            docker tag "${IMAGE}":"${BUILD_VERSION}" "${IMAGE}":latest
-            docker push "${IMAGE}":latest
-        fi
-    fi
-done
+#         if [ "$BRANCH" = "$DEPLOY_BRANCH" ]; then
+#             docker tag "${IMAGE}":"${BUILD_VERSION}" "${IMAGE}":latest
+#             docker push "${IMAGE}":latest
+#         fi
+#     fi
+# done
 
 git config user.name
 git config user.password
