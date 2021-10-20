c971cf5ad5e7b43b50a9633c13c5aa0dee3f9e8c
gerrkoff <antonkofa@gmail.com>
2021-10-21 01:22:14 +0200

    wip

--- ci/version.sh
+++ ci/version.sh
@@ -6,7 +6,7 @@ DATE_CMD=${1:-'date -r '}
 TIME_FORMATTED=$($DATE_CMD$COMMIT_TIME +"%Y%m%d-%H%M%S")
 DEPLOY_BRANCH="staging"
 
-if [ "$BRANCH" != "$DEPLOY_BRANCH" ]; then
+if [ "$BRANCH" == "$DEPLOY_BRANCH" ]; then
     echo $TIME_FORMATTED
 else
     echo $BRANCH
