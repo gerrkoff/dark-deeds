9c73d87155d8d5b9a3f499dd1262c467e2e8b2cf
gerrkoff <antonkofa@gmail.com>
2021-10-21 00:51:27 +0200

    wip

---
+++ ci/version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+COMMIT_TIME=$(git show -s --format=%ct)
+DATE_CMD=${2:-'date -r '}
+TIME_FORMATTED=$($DATE_CMD$COMMIT_TIME +"%Y%m%d-%H%M%S")
+DEPLOY_BRANCH="staging"
+
+if [ "$BRANCH" != "$DEPLOY_BRANCH" ]; then
+    echo $TIME_FORMATTED
+else
+    echo $BRANCH
+fi
