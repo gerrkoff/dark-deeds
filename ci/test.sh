823e2d855c23bef4282c686c1f2a5233f3b6097e
gerrkoff <antonkofa@gmail.com>
2021-10-21 21:28:46 +0200

    wip

--- ci/test.sh
+++ ci/test.sh
@@ -10,14 +10,14 @@ else
     RUN_STAGING="false"
 fi
 
-rm -rf ci/tests/results
-docker build -t dd-test-e2e -f ci/tests/e2e.dockerfile . || exit $?
+rm -rf ci/results
+docker build -t dd-test-e2e -f ci/apps/e2e-tests.dockerfile . || exit $?
 docker run -t --rm \
     -e TZ=America/New_York \
     -e RUN_CONTAINER='true' \
     -e RUN_STAGING=$RUN_STAGING \
     -e ARTIFACTS_PATH='/app/artifacts' \
     -e URL="$1" \
-    -v "$(pwd)"/ci/tests/results:/app/artifacts \
+    -v "$(pwd)"/ci/results:/app/artifacts \
     --name dd-test-e2e \
     dd-test-e2e
