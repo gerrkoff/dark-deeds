ded307530007fff33d0245e96dd1662382d25306
gerrkoff <antonkofa@gmail.com>
2021-10-29 17:39:07 +0200

    wip

--- code/backend/DarkDeeds.TaskServiceApp.Infrastructure/Data/IRepository.cs
+++ code/backend/DarkDeeds.TaskServiceApp.Infrastructure/Data/IRepository.cs
@@ -9,14 +9,12 @@ namespace DarkDeeds.TaskServiceApp.Infrastructure.Data
     public interface IRepository<T>
         where T: IEntity
     {
-        // TODO! save rename
         Task<T> GetByIdAsync(string uid);
         Task<IList<T>> GetBySpecAsync(ISpecification<T> spec);
         Task<IList<T>> GetListAsync();
 
         Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
 
-        // TODO: split on insert & update
         Task UpsertAsync(T entity);
         Task UpdatePropertiesAsync(T entity, params Expression<Func<T, object>>[] properties);
         
