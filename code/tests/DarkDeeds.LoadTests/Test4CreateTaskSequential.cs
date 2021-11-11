82d306d863d83b889e0ff8ef7668702d8a46d461
gerrkoff <antonkofa@gmail.com>
2021-11-12 00:49:55 +0100

    adjust load tests

--- code/tests/DarkDeeds.LoadTests/Test4CreateTaskSequential.cs
+++ code/tests/DarkDeeds.LoadTests/Test4CreateTaskSequential.cs
@@ -9,7 +9,7 @@ namespace DarkDeeds.LoadTests
 {
     public class Test4CreateTaskSequential : BaseTest
     {
-        protected override int Rps => 5;
+        protected override int Rps => 8;
         
         [Fact]
         // [Fact(Skip = "Skip")]
