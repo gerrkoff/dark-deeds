9c486944cf36cbe4c2589cf6e306dcde97dfb269
gerrkoff <antonkofa@gmail.com>
2022-02-10 00:45:24 +0100

    adjust version parsing

--- ci/version.sh
+++ ci/version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH=$(git rev-parse --abbrev-ref HEAD | sed "s/\./-/g" | sed "s/\//-/g" | sed "s/_/-/g" )
 COMMIT_TIME=$(git show -s --format=%ct)
 DATE_CMD=${1:-'date -r '}
 TIME_FORMATTED=$($DATE_CMD$COMMIT_TIME +"%Y%m%d-%H%M%S")
