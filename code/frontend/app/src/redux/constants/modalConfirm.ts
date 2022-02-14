c09d44b1e80199cc2f2d0c97917f6526e4f1f04a
gerrkoff <antonkofa@gmail.com>
2022-02-14 13:52:31 +0100

    wip

--- code/frontend/app/src/redux/constants/modalConfirm.ts
+++ code/frontend/app/src/redux/constants/modalConfirm.ts
@@ -1,7 +1,6 @@
 export const MODALCONFIRM_OPEN = 'MODALCONFIRM_OPEN'
-export type MODALCONFIRM_OPEN = typeof MODALCONFIRM_OPEN
 export interface IModalConfirmOpen {
-    type: MODALCONFIRM_OPEN
+    type: typeof MODALCONFIRM_OPEN
     content: React.ReactNode
     action: () => void
     header?: string
@@ -9,9 +8,8 @@ export interface IModalConfirmOpen {
 }
 
 export const MODALCONFIRM_CLOSE = 'MODALCONFIRM_CLOSE'
-export type MODALCONFIRM_CLOSE = typeof MODALCONFIRM_CLOSE
 export interface IModalConfirmClose {
-    type: MODALCONFIRM_CLOSE
+    type: typeof MODALCONFIRM_CLOSE
 }
 
 export type ModalConfirmAction = IModalConfirmOpen | IModalConfirmClose
