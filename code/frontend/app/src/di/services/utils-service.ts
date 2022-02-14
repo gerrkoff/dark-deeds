49da9290cf5aec0dfe5cc283909ee262d468f5d6
Anton Prokofiev <antonkofa@gmail.com>
2022-02-14 19:23:23 +0100

    Fix UUID issue (#107)

--- code/frontend/app/src/di/services/utils-service.ts
+++ code/frontend/app/src/di/services/utils-service.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid'
+
 export class UtilsService {
     public delay(waitTime: number): Promise<void> {
         return new Promise(resolve => {
@@ -6,11 +8,7 @@ export class UtilsService {
     }
 
     public uuidv4(): string {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-            const r = (Math.random() * 16) | 0
-            const v = c === 'x' ? r : (r & 0x3) | 0x8
-            return v.toString(16)
-        })
+        return uuidv4()
     }
 }
 
