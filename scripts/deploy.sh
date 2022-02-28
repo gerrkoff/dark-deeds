be3dafb59d4d06358acaaf6919df56d5c8fa9bb6
Anton Prokofiev <antonkofa@gmail.com>
2022-03-01 00:33:14 +0100

    fix deploy script (#117)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STATUS=$(git status)
+STATUS=$(LANG=en_US git status)
 if [[ $STATUS != *"Your branch is up to date"* ]] || [[ $STATUS != *"nothing to commit, working tree clean"* ]]
 then
     echo Current branch is not clean
