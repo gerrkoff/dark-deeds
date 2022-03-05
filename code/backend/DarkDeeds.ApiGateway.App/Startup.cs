90b63b559ca40315adab6ebe5b26d917adcc3263
gerrkoff <antonkofa@gmail.com>
2022-03-05 10:29:22 +0100

    add exception handling to api-gateway

--- code/backend/DarkDeeds.ApiGateway.App/Startup.cs
+++ code/backend/DarkDeeds.ApiGateway.App/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Ocelot.Configuration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
@@ -46,6 +47,7 @@ namespace DarkDeeds.ApiGateway.App
             app.UseHealthChecks("/healthcheck");
             app.UseWebSockets();
             app.UseDarkDeedsAppMetrics();
+            app.UseDarkDeedsExceptionHandler(env.IsProduction());
             app.UseDarkDeedsAppMetricsServer();
             app.UseOcelot().Wait();
         }
