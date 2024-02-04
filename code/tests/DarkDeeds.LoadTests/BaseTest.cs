acfca4a292ae859781b90ff31f7f2aaf509b33cb
gerrkoff <antonkofa@gmail.com>
2024-02-04 17:46:53 +0100

    wip

--- code/tests/DarkDeeds.LoadTests/BaseTest.cs
+++ code/tests/DarkDeeds.LoadTests/BaseTest.cs
@@ -28,7 +28,7 @@ namespace DarkDeeds.LoadTests
         private static readonly string DateFolder = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss");
         private static readonly string Domain = Env.Domain;
         private readonly StringBuilder _output = new();
-        protected static readonly string Url = $"https://{Domain}";
+        protected static readonly string Url = $"http://{Domain}";
         protected static readonly Config Config = new();
 
         protected int Timeout => Config.Timeout;
