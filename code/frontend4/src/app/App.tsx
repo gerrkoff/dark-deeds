0d76f8561d306f1a3b3bc2ac6f092e307d3437cb
gerrkoff <antonkofa@gmail.com>
2024-10-14 12:21:10 +0200

    wip

--- code/frontend4/src/app/App.tsx
+++ code/frontend4/src/app/App.tsx
@@ -1,8 +1,5 @@
 import { useCallback } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks'
-import { overviewSelector } from '../overview/redux/overview-selectors'
-import { incrementByAmount } from '../overview/redux/overview-slice'
-import { addWithDelay } from '../overview/redux/overview-thunk'
 import { NavigationBar } from './components/NavigationBar'
 import { appSelector } from './redux/app-selectors'
 import { switchToTab } from './redux/app-slice'
@@ -12,8 +9,6 @@ import { RecurrentTasks } from '../recurrent/RecurrentTasks'
 import { Settings } from '../settings/Settings'
 
 function App() {
-    const count = useAppSelector(state => state.overview.value)
-    const count2 = useAppSelector(overviewSelector)
     const dispatch = useAppDispatch()
 
     const { applicationTab } = useAppSelector(appSelector)
@@ -27,17 +22,6 @@ function App() {
 
     return (
         <div className="container">
-            {count}
-            {count2}
-
-            <button onClick={() => dispatch(incrementByAmount(3))}>
-                Increment
-            </button>
-
-            <button onClick={() => dispatch(addWithDelay(123))}>
-                Increment with delay
-            </button>
-
             {applicationTab === 'overview' && <Overview />}
             {applicationTab === 'recurrent' && <RecurrentTasks />}
             {applicationTab === 'settings' && <Settings />}
