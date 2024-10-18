1d7a2b3345fea16df2f4c06375ebbcf01810098c
gerrkoff <antonkofa@gmail.com>
2024-10-18 09:58:07 +0200

    wip

--- code/frontend4/src/login/hooks/useLoadCurrentUser.ts
+++ code/frontend4/src/login/hooks/useLoadCurrentUser.ts
@@ -3,6 +3,7 @@ import { useAppDispatch } from '../../hooks'
 import { switchToTab } from '../../app/redux/app-slice'
 import { fetchCurrentUser } from '../redux/login-thunk'
 import { unwrapResult } from '@reduxjs/toolkit'
+import { loadSharedSettings } from '../../settings/redux/settings-thunk'
 
 interface Output {
     loadCurrentUser: () => Promise<void>
@@ -14,9 +15,12 @@ export function useLoadCurrentUser(): Output {
     const loadCurrentUser = useCallback(async () => {
         try {
             const fetchCurrentUserResult = await dispatch(fetchCurrentUser())
+
             const currentUserInfo = unwrapResult(fetchCurrentUserResult)
+
             if (currentUserInfo.userAuthenticated) {
                 dispatch(switchToTab('overview'))
+                dispatch(loadSharedSettings())
             } else {
                 dispatch(switchToTab('login'))
             }
