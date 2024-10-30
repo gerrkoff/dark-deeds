f3d9ea1daea838385ffba5b4b876c131fd7722c4
gerrkoff <antonkofa@gmail.com>
2024-10-30 23:45:45 +0100

    wip

--- code/frontend4/src/main.tsx
+++ code/frontend4/src/main.tsx
@@ -5,14 +5,22 @@ import { store } from './store'
 import { Provider } from 'react-redux'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
 
 const root = document.getElementById('root')
+
+const isMobile = () => {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0
+}
+
+const dndBackend = isMobile() ? TouchBackend : HTML5Backend
+const options = isMobile() ? { delay: 500, ignoreContextMenu: true } : {}
 
 if (root) {
     createRoot(root).render(
         <StrictMode>
             <Provider store={store}>
-                <DndProvider backend={HTML5Backend}>
+                <DndProvider backend={dndBackend} options={options}>
                     <App />
                 </DndProvider>
             </Provider>
