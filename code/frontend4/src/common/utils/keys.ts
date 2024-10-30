75c027be198a04ed8e6f829b54d7b2c08a8d2436
gerrkoff <antonkofa@gmail.com>
2024-10-30 20:36:17 +0100

    wip

--- code/frontend4/src/common/utils/keys.ts
+++ code/frontend4/src/common/utils/keys.ts
@@ -1,3 +1,7 @@
-export function isKeyEsc(e: React.KeyboardEvent): boolean {
+export function isKeyEsc(e: React.KeyboardEvent | KeyboardEvent): boolean {
     return e.key === 'Escape'
+}
+
+export function isKeyEnter(e: React.KeyboardEvent | KeyboardEvent): boolean {
+    return e.key === 'Enter'
 }
