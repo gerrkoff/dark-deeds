268cbb62e18d7bb7f29129de3446bc3394251d1e
Anton Prokofev <antonkofa@gmail.com>
2024-10-31 16:36:31 +0100

    fe/new-fe-7 (#225)

--- code/frontend4/vite.config.ts
+++ code/frontend4/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
+        // host: true,
         port: 3000,
         strictPort: true,
     },
