2a6acf8604a7daee0bb71ab31c7a7a13024abcca
gerrkoff <antonkofa@gmail.com>
2024-11-07 00:56:05 +0100

    wip

--- code/frontend4/src/app/components/WelcomeState.tsx
+++ code/frontend4/src/app/components/WelcomeState.tsx
@@ -2,7 +2,10 @@ function WelcomeState() {
     return (
         <div className="position-relative vh-100">
             <div className="position-absolute top-50 start-50 translate-middle">
-                <div className="spinner-grow" role="status">
+                {/* <div className="spinner-grow" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div> */}
+                <div className="spinner-border" role="status">
                     <span className="visually-hidden">Loading...</span>
                 </div>
             </div>
