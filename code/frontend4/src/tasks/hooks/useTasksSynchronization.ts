b33320057c2d6e978645739308f4d162a133a5cc
gerrkoff <antonkofa@gmail.com>
2024-11-06 10:47:06 +0100

    wip

---
+++ code/frontend4/src/tasks/hooks/useTasksSynchronization.ts
@@ -0,0 +1,49 @@
+import { useCallback } from 'react'
+import { useAppDispatch } from '../../hooks'
+import { syncTasks, syncVersions } from '../../overview/redux/overview-slice'
+import { TaskModel } from '../models/TaskModel'
+import { taskSyncService } from '../services/TaskSyncService'
+import { reloadOverviewTasks } from '../../overview/redux/overview-thunk'
+import { unwrapResult } from '@reduxjs/toolkit'
+
+interface Output {
+    processTasksOnlineUpdate: (tasks: TaskModel[]) => void
+    reloadTasks: () => void
+}
+
+export function useTasksSynchronization(): Output {
+    const dispatch = useAppDispatch()
+
+    const processTasksOnlineUpdate = useCallback(
+        (tasks: TaskModel[]) => {
+            const { tasksToNotify, versionsToNotify } =
+                taskSyncService.updateTasks(tasks)
+
+            console.log('__ Tasks Online Update:', {
+                tasks,
+                tasksToNotify,
+                versionsToNotify,
+            })
+
+            if (tasksToNotify.length > 0) {
+                dispatch(syncTasks(tasksToNotify))
+            }
+
+            if (versionsToNotify.length > 0) {
+                dispatch(syncVersions(versionsToNotify))
+            }
+        },
+        [dispatch],
+    )
+
+    const reloadTasks = useCallback(async () => {
+        const reloadOverviewTasksResult = await dispatch(reloadOverviewTasks())
+        const tasks = unwrapResult(reloadOverviewTasksResult)
+        processTasksOnlineUpdate(tasks)
+    }, [dispatch, processTasksOnlineUpdate])
+
+    return {
+        processTasksOnlineUpdate,
+        reloadTasks,
+    }
+}
