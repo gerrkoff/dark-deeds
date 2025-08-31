278ed478c5dc899cc9363840577543114bdbdda6
gerrkoff <antonkofa@gmail.com>
2025-08-31 19:25:31 +0200

    feat: Add extra_hosts configuration for test-e2e-chrome service and update test script environment variables

--- infra/test.sh
+++ infra/test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+export URL="http://host.docker.internal:3000"
+export BE_URL="http://localhost:5000"
+export SELENIUM_GRID_URL="http://localhost:4444/"
+export CONTAINER="true"
+
 dotnet test -v normal ./code/tests/DarkDeeds.E2eTests/DarkDeeds.E2eTests.csproj
