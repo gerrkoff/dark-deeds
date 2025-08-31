567355f34865e4be6dbb5f80f4fc803af593c658
gerrkoff <antonkofa@gmail.com>
2025-08-31 21:53:23 +0000

    feat: Update test script to dynamically set backend URL based on availability

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export URL="http://host.docker.internal:3000"
-export BE_URL="http://localhost:5000"
+if curl -s --connect-timeout 2 http://host.docker.internal:5000 > /dev/null 2>&1; then
+    export BE_URL="http://host.docker.internal:5000"
+else
+    export BE_URL="http://localhost:5000"
+fi
 export SELENIUM_GRID_URL="http://localhost:4444/"
 export CONTAINER="true"
 
