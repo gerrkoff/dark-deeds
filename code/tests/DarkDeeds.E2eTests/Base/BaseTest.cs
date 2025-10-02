f29258f02360acee18c573ba402c937c55a346fc
gerrkoff <antonkofa@gmail.com>
2025-10-02 00:32:32 +0000

    refactor: Remove unnecessary using directives from BaseTest files

--- code/tests/DarkDeeds.E2eTests/Base/BaseTest.cs
+++ code/tests/DarkDeeds.E2eTests/Base/BaseTest.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Text.Json;
 using DarkDeeds.E2eTests.Common;
--- code/tests/DarkDeeds.LoadTests/BaseTest.cs
+++ code/tests/DarkDeeds.LoadTests/BaseTest.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Net;
 using System.Net.Mime;
